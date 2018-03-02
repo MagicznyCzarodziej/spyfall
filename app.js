509594e6f495ea668554fcec9f5b3bd227aebc3b
Magiczny Czarodziej <nexit07@gmail.com>
2018-03-02 23:04:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,163 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const rooms = {};
+const users = {}; // {userId:roomId/null}
+
+// Game
+const locations = [{place: 'Szpital', roles: ['Pięlęgniarka', 'Lekarz', 'Chirurg', 'Kierownik oddziału', 'Salowa', 'Pacjent'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Restauracja', roles: ['Szef kuchni', 'Kucharz', 'Właściciel', 'Magda Gessler', 'Gość', 'Gość', 'Bloger kulinarny'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Statek pasażerski', roles: ['Kucharz', 'Pasażer', 'Pasażer', 'Kapitan', 'Członek załogi', 'Mechanik'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Zoo', roles: ['Zwiedzający', 'Dyrektor', 'Karmiciel zwierząt', 'Sprzątacz', 'Przewodnik', 'Kierownik ochrony'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Łódź podwodna', roles: ['Kapitan', 'Żołnierz', 'Nawigator', 'Radiowec', 'Mechanik', 'Członek załogi'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Szkoła', roles: ['Uczeń', 'Uczeń', 'Nauczyciel', 'Nauczyciel', 'Sprzątaczka', 'Dyrektor', 'Kierownik', 'Konserwator', 'Psycholog'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Stacja kosmiczna', roles: ['Astronauta', 'Kosmonauta', 'Astronauta', 'Kosmonauta','Astronauta', 'Tajkonauta'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Teatr', roles: ['Aktor', 'Aktor', 'Aktor', 'Reżyser', 'Scenarzysta', 'Charakteryzator', 'Oświetleniowiec', 'Widz'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Ambasada', roles: ['Kierownik ochrony', 'Sprzątaczka', 'Pracownik', 'Konsul', 'Księgowa', 'Sekretarz'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Samolot', roles: ['Pasażer', 'Pasażer', 'Stewardessa', 'Pilot', 'Drugi pilot', 'Tajny ochroniarz'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Bank', roles: ['Kasjer', 'Ochroniarz', 'Klient', 'Konwojent', 'Rabuś'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Kościół', roles: ['Ksiądz', 'Ministrant', 'Kościelny', 'Organista', 'Biskup', 'Wierny'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Namiot cyrkowy', roles: ['Klaun', 'Treser zwierząt', 'Człowiek guma', 'Widz', 'Połykacz ostrzy', 'Żongler'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Korporacja', roles: ['Prezes', 'Korposzczur', 'Sprzątaczka', 'Sekretarka', 'Manager', 'Dyrektor', 'Specjalista PR'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Stacja polarna', roles: ['Naukowiec', 'Naukowiec', 'Lekarz', 'Badacz', 'Klimatolog', 'Geolog'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Supermarket', roles: ['Kasjerka', 'Ochroniarz', 'Klient', 'Prezes', 'Kierownik', 'Sprzątaczka'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Komisariat', roles: ['Policjant', 'Komendant', 'Aspirant', 'Porucznik', 'Ojciec Mateusz', 'Detektyw', 'Konfident'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Plan filmowy', roles: ['Reżyser', 'Scenarzysta', 'Scenograf', 'Charakteryzator', 'Aktor', 'Operator kamery'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Baza wojskowa', roles: ['Żołnierz', 'Czołgista', 'Snajper', 'Podporucznik', 'Szeregowy', 'Pułkownik'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Kasyno', roles: ['Gracz', 'Kierownik ochrony', 'Prezes', 'Krupier', 'Hostessa', 'Pokerzysta'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Pociąg pasażerski', roles: ['Konduktor', 'Pasażer', 'Maszynista', ''], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Statek piratów', roles: ['Pirat', 'Bosman', 'Czarnobrody', 'Majtek', 'Żółtodziób', 'Kpt. Jack Sparrow'], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Hotel', roles: ['Gość', 'Gość', 'Recepcjonistka', 'Sprzątaczka', ''], imageUrl: 'http://via.placeholder.com/300x150'},
+{place: 'Plaża', roles: ['Plażowicz', 'Plażowicz', 'Sprzedawca hotdogów', 'Nudysta', 'Ratownik', 'Polak z parawanem'], imageUrl: 'http://via.placeholder.com/300x150'}];
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('IO | User connected');
+  let addedUser = false;
+
+  // Register user
+  socket.on('username', (username) => {
+    if (addedUser) return;
+
+    socket.username = username;
+    users[socket.id] = null;
+    addedUser = true;
+    console.log(`IO | User nickname: ${username} ${socket.id}`);
+  });
+
+  // Room created
+  socket.on('create-room', () => {
+    const roomId = socket.username.slice(0,3) + '_' + Math.round(Math.random()*999999+100000);
+    console.log(`IO | Room ${roomId} created by: ${socket.username} (${socket.id})`);
+    rooms[roomId] = {adminId: socket.id, users: [{userId: socket.id, username: socket.username}]};
+
+    users[socket.id] = roomId;
+    socket.join(roomId);
+    socket.emit('room-created', roomId);
+    socket.broadcast.emit('new-room', roomId);
+  });
+
+  // Room closed
+  socket.on('close-room', () => {
+    const userRoom = users[socket.id];
+    if (userRoom !== null && rooms[userRoom].adminId === socket.id) {
+      delete rooms[userRoom];
+      users[socket.id] = null;
+      socket.leave(userRoom);
+      socket.broadcast.emit('closed-room', userRoom); //Tell all users about closed room
+      console.log(`IO | Room closed: ${userRoom}`);
+    }
+  });
+
+  // Show rooms
+  socket.on('show-rooms', () => {
+    socket.emit('rooms-list', Object.keys(rooms));
+  });
+
+  // Leave room
+  socket.on('leave-room', () => {
+    const userRoom = users[socket.id];
+    if (userRoom in rooms) {
+      const userIndex = rooms[userRoom].users.map(function(e) { return e.userId; }).indexOf(socket.id);
+      rooms[userRoom].users.splice(userIndex, 1);
+      io.to(userRoom).emit('user-left', socket.id);
+    }
+    users[socket.id] = null;
+    socket.leave(userRoom);
+  });
+
+  // Join lobby
+  socket.on('join-room', (roomId) => {
+    if (roomId in rooms) {
+      socket.join(roomId);
+      users[socket.id] = roomId;
+      rooms[roomId].users.push({userId: socket.id, username: socket.username});
+      socket.emit('joined-room', rooms[roomId].users)
+      socket.broadcast.to(roomId).emit('user-joined', {userId: socket.id, username: socket.username});
+    } else socket.emit('join-failed');
+  });
+  // Start game
+  socket.on('start-game', (time) => {
+    const userRoom = users[socket.id];
+    if (rooms[userRoom].adminId !== socket.id) return;
+
+    // Choose random location
+    const location = locations[Math.floor(Math.random()*locations.length)];
+    const locationName = location.place;
+    const locationImage = location.imageUrl;
+    const roles = location.roles;
+
+    const usersIds = rooms[userRoom].users.map((user) => user.userId);
+    const usersNumber = usersIds.length;
+    // Choose spy
+    const spyIndex = Math.floor(Math.random()*usersNumber);
+    const spyId = usersIds.splice(spyIndex, 1);
+    console.log("Spy index: " + spyIndex + ", spyId: " + spyId);
+    io.sockets.to(spyId).emit('game-started', {isSpy: true, time: time});
+
+    // Choose roles for the rest of the players
+    usersIds.forEach((userId) => {
+      const roleIndex = Math.floor(Math.random()*roles.length);
+      const role = roles.splice(roleIndex, 1);
+
+      io.sockets.to(userId).emit('game-started', {isSpy: false, location: locationName, role: role, imageUrl: locationImage, time: time});
+      io.sockets.connected[userId].leave(userRoom);
+      users[userId] = null;
+    });
+
+    //Close room
+    delete rooms[userRoom];
+  });
+
+  // Disconnect
+  socket.on('disconnect', () => {
+    if (!addedUser) return;
+    // Leave room
+    const userRoom = users[socket.id];
+    if (userRoom in rooms) {
+      const userIndex = rooms[userRoom].users.map(function(e) { return e.userId; }).indexOf(socket.id);
+      rooms[userRoom].users.splice(userIndex, 1);
+      io.to(userRoom).emit('user-left', socket.id);
+    }
+
+    socket.leave(userRoom);
+
+    // Close room
+    if (userRoom !== null && rooms[userRoom].adminId === socket.id) { // <- Cousing error on leaving game
+      delete rooms[userRoom];
+      socket.broadcast.emit('closed-room', userRoom); //Tell all users about closed room
+      console.log(`IO | Room closed: ${userRoom}`);
+    }
+    users[socket.id] = null;
+
+    console.log(`IO | User disconnected: ${socket.username}`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
---
+++ public/client.js
@@ -0,0 +1,171 @@
+$(() => {
+  const socket = io();
+  let username = 'Anonim';
+  let isAdmin = false;
+  let myRoomId = null;
+  let tryingToJoin = false;
+
+  function changePage(page) {
+    $('.page').hide();
+    $('#' + page).show();
+
+    switch(page) {
+      case 'home': break;
+      case 'lobby':
+        $('#players-list').html(''); // Clear players list
+        if (isAdmin) {
+          $('.admin').show();
+          $('.not-admin').hide();
+        } else {
+          $('.admin').hide();
+          $('.not-admin').show();
+        }
+        break;
+      case 'game':
+        $('.location').removeClass('selected'); // Clear selections from last game
+        break;
+    }
+  }
+
+  // Create lobby
+  $('#create').on('click', () => {
+    username = $('#username-input').val();
+    if(username.length < 3) return; // Change this to show error
+
+    socket.emit('username', username);
+    socket.emit('create-room');
+  });
+
+  socket.on('room-created', (roomId) => {
+    isAdmin = true;
+    myRoomId = roomId;
+    changePage('lobby');
+    $('#players-list').html(`<div class="player" data-user_id="admin">${username}</div>`)
+  });
+
+  // Close lobby
+  $('#cancel-lobby').on('click', () => {
+    if (isAdmin) {
+      socket.emit('close-room');
+      isAdmin = false;
+    } else socket.emit('leave-room');
+    myRoomId = null;
+    changePage('home');
+  });
+
+  // Show rooms list
+  $('#show-rooms').on('click', () => {
+    username = $('#username-input').val();
+    if(username.length < 3) return; // Change this to show error
+    socket.emit('username', username);
+    socket.emit('show-rooms');
+    changePage('rooms');
+  });
+
+  socket.on('rooms-list', (rooms) => {
+    if (rooms.length === 0) $('#rooms-label').text('Brak pokoi');
+    else {
+      $('#rooms-label').text('Wybierz pokój:');
+      $('#rooms-list').html('');
+    }
+    rooms.forEach((room) => {
+      $('#rooms-list').append(`<div class="room" data-id=${room}>${room}</div>`);
+    });
+  });
+
+  // Close rooms list
+  $('#cancel-rooms').on('click', () => {
+    tryingToJoin = false;
+    myRoomId = null;
+    changePage('home')
+  });
+
+  // Join lobby
+  $('#rooms-list').on('click', '.room', function () {
+    const roomId = $(this).data('id');
+    tryingToJoin = true;
+    myRoomId = roomId;
+    socket.emit('join-room', roomId);
+  });
+
+  socket.on('join-failed', () => {
+    myRoomId = null;
+    console.log('Dołączanie nieudane');
+  });
+
+  socket.on('joined-room', (users) => {
+    if (!tryingToJoin) return;
+    isAdmin = false;
+    tryingToJoin = false;
+
+    changePage('lobby');
+    console.log(users);
+    users.forEach((user) => {
+      $('#players-list').append(`<div class="player" data-user_id=${user.userId}>${user.username}</div>`)
+    });
+  });
+
+  // User joined
+  socket.on('user-joined', (user) => {
+    $('#players-list').append(`<div class="player" data-user_id=${user.userId}>${user.username}</div>`)
+  });
+
+  // User left
+  socket.on('user-left', (userId) => {
+    $(`.player[data-user_id=${userId}]`).fadeOut(500, function () {
+      $(this).remove();
+    });
+  });
+
+  // New room
+  socket.on('new-room', (roomId) => {
+    $('#rooms-list').fadeOut(300, function () {
+      $(this).prepend(`<div class="room" data-id=${roomId}>${roomId}</div>`).fadeIn(200);
+    });
+  });
+
+  // Room closed
+  socket.on('closed-room', (roomId) => {
+    if (roomId === myRoomId) {
+      socket.emit('leave-room');
+      myRoomId = null;
+      changePage('home');
+    }
+
+    $(`.room[data-id=${roomId}]`).fadeOut(500, function () {
+      $(this).remove();
+    });
+  });
+
+  // Start game
+  $('#start').on('click', () => {
+    const time = $('input[name="time-input"]').val();
+    socket.emit('start-game', time);
+  });
+
+  socket.on('game-started', (data) => {
+    myRoomId = null;
+    changePage('game');
+    $('#player-name').html(`Imię: <span>${username}</span>`);
+    const isSpy = data.isSpy;
+    const time = data.time;
+    $('#time-left').html(time);
+    if (isSpy) {
+      $('#player-role').html('<span>Jesteś</span> szpiegiem');
+      $('#player-location').html('');
+      $('#player-location').hide();
+    } else {
+      const role = data.role;
+      const location = data.location;
+      const imageUrl = data.imageUrl;
+      $('#player-role').html(`Postać: <span>${role}</span>`);
+      $('#player-location').show().css('background-image', `url("${imageUrl}")`);
+      $('#player-location').html(`Miejsce: <span>${location}</span>`);
+    }
+  });
+
+  // Check location
+  $('.location').on('click', function() {
+    $(this).toggleClass('selected');
+  });
+}); //DOM Ready
